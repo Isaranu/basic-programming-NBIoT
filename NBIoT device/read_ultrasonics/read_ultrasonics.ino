840d9a1ad513794fcf949627345e3cd075748ac9
Isaranu Janthong <isaranujanthong@yahoo.com>
2019-06-10 22:25:39 +0700

    Update read_ultrasonics.ino

--- NBIoT device/read_ultrasonics/read_ultrasonics.ino
+++ NBIoT device/read_ultrasonics/read_ultrasonics.ino
@@ -1,1 +1,19 @@
+#include <Ultrasonic.h>
 
+Ultrasonic ultrasonic(11,12); /* (Trig PIN,Echo PIN) */
+int distances;
+
+void setup(){
+
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+   /* Send Ultrasonic distances as every cycleTime */
+   distances = ultrasonic.distanceRead();
+   Serial.println(distances); 
+
+   delay(50);
+}
